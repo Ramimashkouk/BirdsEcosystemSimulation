786485988396e8caa20605a35e112bc7eb6023b0
Ramimashkouk <ramima1996@gmail.com>
2021-12-16 06:33:10 +0300

    Adding_bird_type

--- test.py
+++ test.py
@@ -10,26 +10,35 @@ def randomize_values(lst):
     return lst
 
 population = Population(INIT_SUCKERS)
-population.add_cheat(N_CHEATS)
+for _ in range(INIT_GRUDGERS):
+    population.add_bird('grudger')
+for _ in range(N_CHEATS):
+    population.add_bird('cheat')
 
 generation = 0
 n_suckers_by_generations = []
 n_cheats_by_generations = []
+n_grudgers_by_generations = []
 
-def plot(x_val, x_val_cheat, y_val, y_val_cheat):
+def plot(x_val, x_val_cheat, x_grudgers, y_val, y_val_cheat, y_grudgers):
     x_val = randomize_values(x_val)
     y_val = randomize_values(y_val)
     x_val_cheat = randomize_values(x_val_cheat)
     y_val_cheat = randomize_values(y_val_cheat)
+    x_grudgers = randomize_values(x_grudgers)
+    y_grudgers = randomize_values(y_grudgers)
     
     print('# of Suckers', len(x_val))
     print('# of Cheats', len(x_val_cheat))
     n_suckers_by_generations.append(len(x_val))
     n_cheats_by_generations.append(len(x_val_cheat))
+    n_grudgers_by_generations.append(len(x_grudgers))
     
     ax1.cla()
     ax1.scatter(y_val, x_val, c='b', alpha=0.4)
     ax1.scatter(y_val_cheat, x_val_cheat, c='r', marker='s', alpha=0.4)
+    ax1.scatter(y_grudgers, x_grudgers, c='g', marker='*', alpha=0.4)
+
 
     ax1.set_title('Birds of the ' + str(generation) + ' generations')
     ax1.set_xlabel('Current age')
@@ -38,10 +47,11 @@ def plot(x_val, x_val_cheat, y_val, y_val_cheat):
     ax2.cla()
     ax2.plot(range(generation), n_suckers_by_generations, marker='o', c='b')
     ax2.plot(range(generation), n_cheats_by_generations, marker='s', c='r')
+    ax2.plot(range(generation), n_grudgers_by_generations, marker='*', c='g')
     ax2.set_title('# of birds per generation')
     ax2.set_xlabel('Generation')
     ax2.set_ylabel('Number of birds')
-    ax2.legend(['Sucker', 'Cheat'])
+    ax2.legend(['Sucker', 'Cheat', 'Grudger'])
 
 stop=0
 def gen():
@@ -57,10 +67,10 @@ def animate(i):
     print('\nGeneration', generation)
     print('Population is ', len(population.population))
     
-    x_val, x_val_cheat = population.get_birds_fitness()
-    y_val, y_val_cheat = population.ages()
+    x_val, x_val_cheat, x_grudgers = population.get_birds_attribute('fitness')
+    y_val, y_val_cheat, y_grudgers = population.get_birds_attribute('age')
 
-    plot(x_val, x_val_cheat, y_val, y_val_cheat)
+    plot(x_val, x_val_cheat, x_grudgers, y_val, y_val_cheat, y_grudgers)
     
     try:
         population.get_fitness()
@@ -68,11 +78,16 @@ def animate(i):
         print('Birds are extinct')
         stop=1
     population.reproduce()
-    population.disease()
-    population.azrael_turn()
+    # Grudgers benifits more when the disease happens more frequently in the same generation
+    for _ in range(N_DISEASE_PER_GEN):
+        population.disease()
+        population.azrael_turn()
+    if generation == 150:
+        stop=1
 
 
 fig, (ax1, ax2) = plt.subplots(1,2)
+fig.suptitle('INFECTION_RATE = '+ str(INFECTION_RATE)+ ', MUTATION_RATE = '+ str(MUTATION_RATE) +  ', MAX_LIFESPAN = '+str(MAX_LIFESPAN) + ',\n MAX_POPULATION = ' + str(MAX_POPULATION) + ', MAX_HELP_REQUESTS = '+str(MAX_HELP_REQUESTS) + ', GAIN_SAVING_LIFE = '+ str(GAIN_SAVING_LIFE) + ',\n LOSE_WASTING_TIME = ' +str(LOSE_WASTING_TIME))
 ani = animation.FuncAnimation(fig, animate, frames=gen, interval =1000, repeat=False)
 plt.tight_layout(pad=1.6)
 plt.show()
